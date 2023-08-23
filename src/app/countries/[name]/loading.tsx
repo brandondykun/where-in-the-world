98b931925883323a7b0e593e982dfeee3a6e7e84
Brandon Dykun <brandon.dykun@barnstormresearch.com>
2023-08-23 11:38:46 -0400

    Added loading screen for country page.

---
+++ src/app/countries/[name]/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <main className="flex flex-1 justify-center bg-very-light-gray dark:bg-very-dark-blue-bg items-center">
+      <div className="text-3xl animate-pulse">Loading...</div>
+    </main>
+  );
+};
+
+export default Loading;
