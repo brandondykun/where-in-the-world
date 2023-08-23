52ec62e6e3958a333a7966f15f25881ef64fc29a
Brandon Dykun <brandon.dykun@barnstormresearch.com>
2023-08-23 15:55:51 -0400

    Added error handling.

--- src/app/countries/page.tsx
+++ src/app/countries/page.tsx
@@ -24,12 +24,22 @@ const Countries = () => {
   const [countries, setCountries] = useState<CountryType[] | null>(null);
   const [filtered, setFiltered] = useState<CountryType[]>([]);
   const [regionFilter, setRegionFilter] = useState<string>("All");
+  const [fetchError, setFetchError] = useState(false);
 
   useEffect(() => {
-    getCountries().then((res) => {
-      setCountries(res);
-      setFiltered(res);
-    });
+    setFetchError(false);
+    getCountries()
+      .then((res) => {
+        setCountries(
+          res.sort((a, b) => {
+            return a.name.common.localeCompare(b.name.common);
+          })
+        );
+        setFiltered(res);
+      })
+      .catch((error) => {
+        setFetchError(true);
+      });
   }, []);
 
   // filter visible countires based on search input
@@ -68,8 +78,8 @@ const Countries = () => {
   });
 
   return (
-    <main className="flex-1 flex flex-col bg-very-light-gray dark:bg-very-dark-blue-bg px-4 py-8 md:px-8 md:py-12">
-      <div className="flex flex-col gap-10 pb-8 md:flex-row md:justify-between md:items-center md:pb-16">
+    <main className="flex-1 flex flex-col bg-very-light-gray dark:bg-very-dark-blue-bg px-8 py-8 md:py-12">
+      <div className="flex flex-col gap-10 pb-12 md:flex-row md:justify-between md:items-center md:pb-16">
         <div className="relative max-w-[400px] md:w-96">
           <AiOutlineSearch size={25} className="absolute top-3 left-5" />
           <Input
@@ -106,20 +116,25 @@ const Countries = () => {
       {doubleFiltered.length === 0 && countries !== null ? (
         <div className="text-3xl pt-20 mx-auto">No Countries Found</div>
       ) : null}
-      {/* {countries === null ? ( */}
-      <div className="animate-pulse grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-16 place-items-stretch max-w-[2400px] mx-auto">
-        {Array(200)
-          .fill(0)
-          .map((c, i) => {
-            return (
-              <div
-                className="rounded bg-white dark:bg-dark-blue shadow-md h-[414px] w-[320px]"
-                key={i}
-              ></div>
-            );
-          })}
-      </div>
-      {/* ) : null} */}
+      {countries === null && !fetchError ? (
+        <div className="animate-pulse grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-16 place-items-stretch max-w-[2400px] mx-auto">
+          {Array(200)
+            .fill(0)
+            .map((c, i) => {
+              return (
+                <div
+                  className="rounded bg-white dark:bg-dark-blue shadow-md h-[414px] w-[200px] xs:w-[250px] sm:w-[350px] md:w-[300px] lg:w-[280px] xl:w-[260px] overflow-hidden"
+                  key={i}
+                ></div>
+              );
+            })}
+        </div>
+      ) : null}
+      {fetchError ? (
+        <div className="text-2xl mx-auto pt-24">
+          There was an error fetching the countries. Please try again.
+        </div>
+      ) : null}
     </main>
   );
 };
