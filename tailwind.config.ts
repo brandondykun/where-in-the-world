af69c8169eb6166371ed87c704d7c5d6953a3984
Brandon Dykun <brandon.dykun@barnstormresearch.com>
2023-08-23 11:36:29 -0400

    Added screen break xs size.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  // darkMode: "class",
-  // content: [
-  //   "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-  //   "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  //   "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  // ],
   darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,24 +11,6 @@ const config: Config = {
   future: {
     hoverOnlyWhenSupported: true,
   },
-  // theme: {
-  //   extend: {
-  //     backgroundImage: {
-  //       "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-  //       "gradient-conic":
-  //         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-  //     },
-  //     colors: {
-  //       "dark-blue": "hsl(209, 23%, 22%)",
-  //       "very-dark-blue-bg": "hsl(207, 26%, 17%)",
-  //       "very-dark-blue-text": "hsl(200, 15%, 8%)",
-  //       "dark-gray": "hsl(0, 0%, 52%)",
-  //       "very-light-gray": "hsl(0, 0%, 98%)",
-  //       white: "hsl(0, 0%, 100%)",
-  //     },
-  //   },
-  // },
-  // plugins: [],
   theme: {
     container: {
       center: true,
@@ -65,6 +41,9 @@ const config: Config = {
         "dark-gray": "hsl(0, 0%, 52%)",
         "very-light-gray": "hsl(0, 0%, 98%)",
         white: "hsl(0, 0%, 100%)",
+      },
+      screens: {
+        xs: "300px",
       },
     },
   },
