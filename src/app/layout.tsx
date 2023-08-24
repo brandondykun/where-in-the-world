26bcaadaf38f21f0ec31df3b30891998688af501
Brandon Dykun <brandon.dykun@barnstormresearch.com>
2023-08-24 12:13:32 -0400

    Added width to html.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="w-screen">
       <body
         className={`flex flex-col w-screen min-h-screen ${nunito.className}`}
       >
